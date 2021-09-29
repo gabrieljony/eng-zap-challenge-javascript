d9ec84304ceddfa3411d97e87f50daf04a43ad91
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-09-29 19:06:49 -0300

    aplicado regra de elegibilidade do imóvel para os portais

--- src/app/service/products.service.ts
+++ src/app/service/products.service.ts
@@ -9,6 +9,11 @@ export class ProductsService {
 
   private url: string = "http://grupozap-code-challenge.s3-website-us-east-1.amazonaws.com/sources/source-1.json"
 
+  minlon = -46.693419;
+  minlat = -23.568704;
+  maxlon = -46.641146;
+  maxlat = -23.546686;
+
   constructor(private http: HttpClient) { }
 
   getListProducts(type: string) {
@@ -16,21 +21,40 @@ export class ProductsService {
       .get<any>(this.url)
       .toPromise()
       .then((response: ProductObject[]) => {
-        response = this.ineligibility(response);
+        response = this.ineligibilityLatLon(response);
         if (type === 'zap') {
-          return response.filter(item =>
-            item.pricingInfos.businessType === 'SALE' && Number(item.pricingInfos.price) >= 600000 ||
+          response = response.filter(item =>
+            item.pricingInfos.businessType === 'SALE' && Number(item.pricingInfos.price) >= 600000 - 600000 * (this.boundingBox(item.address.geoLocation.location) ? 0.1 : 0) ||
             item.pricingInfos.businessType === 'RENTAL' && Number(item.pricingInfos.rentalTotalPrice) >= 3500)
+          response = this.eligibilityAreas(response);
         } else if (type === 'vivareal') {
-          return response.filter(item =>
+          response = response.filter(item =>
             item.pricingInfos.businessType === 'SALE' && Number(item.pricingInfos.price) <= 700000 ||
-            item.pricingInfos.businessType === 'RENTAL' && Number(item.pricingInfos.rentalTotalPrice) <= 4000)
+            item.pricingInfos.businessType === 'RENTAL' && Number(item.pricingInfos.rentalTotalPrice) <= 4000 + 4000 * (this.boundingBox(item.address.geoLocation.location) ? 0.5 : 0))
+          response = this.eligibilityValue(response);
         }
         return response;
       });
   }
 
-  ineligibility(list: ProductObject[]): ProductObject[] {
+  ineligibilityLatLon(list: ProductObject[]): ProductObject[] {
     return list.filter(item => !(item.address.geoLocation.location.lat === 0 && item.address.geoLocation.location.lon === 0))
+  }
+
+  eligibilityAreas(list: ProductObject[]): ProductObject[] {
+    return list.filter(item => item.pricingInfos.businessType === 'SALE' && item.usableAreas > 0 && Number(item.pricingInfos.price) / item.usableAreas > 3500)
+  }
+
+  eligibilityValue(list: ProductObject[]): ProductObject[] {
+    return list.filter(item => {
+      console.log('1 - ', Number(item.pricingInfos.monthlyCondoFee), ' - ', item.usableAreas)
+      // console.log('2 - ', !(Number(item.pricingInfos.monthlyCondoFee) >= 0.3 * Number(item.pricingInfos.rentalTotalPrice)))
+      return !Number.isNaN(Number(item.pricingInfos.monthlyCondoFee)) && !(Number(item.pricingInfos.monthlyCondoFee) >= 0.3 * Number(item.pricingInfos.rentalTotalPrice))
+    })
+  }
+
+  boundingBox(location: { lon: number, lat: number }): boolean {
+    let result = location.lon >= this.minlon && location.lon <= this.maxlon && location.lat >= this.minlat && location.lon <= this.maxlat;
+    return result
   }
 }
