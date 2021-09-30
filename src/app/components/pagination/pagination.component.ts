ae984142ff0bda2234f4b1b307a190510a1f486e
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-09-30 10:56:19 -0300

    Melhoria na paginação

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -12,6 +12,8 @@ export class PaginationComponent implements OnInit {
   sub: Subscription;
   page: number;
   produtosPorPagina: number = 20;
+  btnFirst = false;
+  btnLast = false;
 
   @Input() productsTotal: number;
 
@@ -24,25 +26,31 @@ export class PaginationComponent implements OnInit {
         this.page = queryParams['page'];
       }
     )
+    if (this.page <= 1) {
+      this.btnFirst = true;
+    }
+    if (this.page >= this.paginasTotal()) {
+      this.btnLast = true;
+    }
   }
 
   paginas() {
     const current = Number(this.page);//7
     const range = 9;
     const offset = Math.ceil(range / 2);
-    console.log('offset', offset)
+    // console.log('offset', offset)
     const total = this.paginasTotal();
-    console.log('total', total)
+    // console.log('total', total)
     const pagesArray = [];
 
     for (let i = 1; i <= total; i++) {
       pagesArray.push(i);
     }
 
-    pagesArray.splice(0, current - offset);
-    pagesArray.splice(range, total);
+    // pagesArray.splice(0, current - offset);
+    // pagesArray.splice(range, total);
 
-    console.log('pagesArray', pagesArray)
+    // console.log('pagesArray', pagesArray)
     return pagesArray;
   }
 
@@ -51,8 +59,21 @@ export class PaginationComponent implements OnInit {
     return total !== Infinity ? Math.ceil(total) : 0;
   }
 
-  nextPage(page) {
-    this.router.navigate([`/products/${this.route.snapshot.params['slug']}`], { queryParams: { 'page': page } })
+  nextPage(page, type: string = null) {
+    if (type != null) {
+      page = type === 'subt' ? --page : ++page;
+    }
+    this.btnFirst = false;
+    this.btnLast = false;
+    if (page > 0 && page <= this.paginasTotal()) {
+      this.router.navigate([`/products/${this.route.snapshot.params['slug']}`], { queryParams: { 'page': page } })
+    }
+    if (page <= 1) {
+      this.btnFirst = true;
+    }
+    if (page >= this.paginasTotal()) {
+      this.btnLast = true;
+    }
   }
 
   ngOnDestroy() {
